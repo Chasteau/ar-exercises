1136068837418aa526ccc8f4dc659aef0866d1fc
Chasteau <davidchasteau@gmail.com>
2017-07-05 19:41:08 +0000

    completed exercise 5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,8 +11,11 @@ store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: fals
 store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
+rich = Store.find_by_name("Richmond")
+rich.update(womens_apparel: true)
+
 mens_stores = Store.all.where(mens_apparel: true)
 mens_stores.each {|store| puts store = "Store: #{store.name} => Ar: #{store.annual_revenue}"}
 
-womens_store = Store.all.where(womens_apparel: true).where('annual_revenue >= 100000')
+womens_store = Store.all.where(womens_apparel: true).where('annual_revenue > 1000000')
 womens_store.each{|store| puts store = "Store: #{store.name} => Ar: #{store.annual_revenue}"}
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+all_stores = Store.all
+sum = 0
+all_stores.each {|store| sum += store.annual_revenue }
+puts "Total Revenue of #{sum} for #{all_stores.count} stores"
+
+avg = sum/all_stores.count
+puts "Average Revenue of #{avg} across all stores"
+
+mill_plus = Store.all.where('annual_revenue > 1000000')
+puts "There are #{mill_plus.count} stores with $1M in annual revenue"
