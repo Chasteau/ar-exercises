8f033e0c790c1ef618915342349cc9fb1d42daee
Chasteau <davidchasteau@gmail.com>
2017-07-06 01:18:21 +0000

    added employees to stores and added global instances

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Man", last_name: "Boy", hourly_rate: 76)
+@store1.employees.create(first_name: "Girl", last_name: "Woman", hourly_rate: 90)
+
+@store2.employees.create(first_name: "Whale", last_name: "Fish", hourly_rate: 60)
+@store2.employees.create(first_name: "Dog", last_name: "Owl", hourly_rate: 76)
+@store2.employees.create(first_name: "Ink", last_name: "Pen", hourly_rate: 90)
+
